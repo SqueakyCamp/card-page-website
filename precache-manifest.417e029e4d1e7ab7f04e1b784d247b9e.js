c9b512ad433913ae043921649dafefe152bf24c1
Daniel <squeakycamp@gmail.com>
2020-08-08 13:09:55 +0200

    Updates

---
+++ precache-manifest.417e029e4d1e7ab7f04e1b784d247b9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16925bb650e97736e4e946f4d56a3941",
+    "url": "/card-page-website/index.html"
+  },
+  {
+    "revision": "cbe42e7077c38253084f",
+    "url": "/card-page-website/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2a0a15107d4136826785",
+    "url": "/card-page-website/static/css/main.7828f1d3.chunk.css"
+  },
+  {
+    "revision": "cbe42e7077c38253084f",
+    "url": "/card-page-website/static/js/2.907611aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-page-website/static/js/2.907611aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0a15107d4136826785",
+    "url": "/card-page-website/static/js/main.3168651b.chunk.js"
+  },
+  {
+    "revision": "8a4aad32f93a18c0f9c0",
+    "url": "/card-page-website/static/js/runtime-main.18ecaf58.js"
+  },
+  {
+    "revision": "6be08049b7354cd3f284e8f51b7347b8",
+    "url": "/card-page-website/static/media/bg-picture.6be08049.jpg"
+  }
+]);
